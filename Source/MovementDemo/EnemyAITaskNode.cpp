bf324638c4f8449ef9167f3a892c99bae9be4ed7
pew-pew <pew-pew@users.noreply.github.com>
2022-07-23 23:56:05 +0300

    Do not crash if no patrol points

--- Source/MovementDemo/EnemyAITaskNode.cpp
+++ Source/MovementDemo/EnemyAITaskNode.cpp
@@ -8,26 +8,24 @@
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UEnemyAITaskNode::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-  AEnemyAIPatrolController* AIContoller = Cast<AEnemyAIPatrolController>(OwnerComp.GetAIOwner());
-  if (AIContoller) {
-    UBlackboardComponent* BBComp = AIContoller->GetBlackboardComponent();
+	AEnemyAIPatrolController* AIContoller = Cast<AEnemyAIPatrolController>(OwnerComp.GetAIOwner());
+	if (!AIContoller) {
+		return EBTNodeResult::Failed;
+	}
+	UBlackboardComponent* BBComp = AIContoller->GetBlackboardComponent();
 
-    AEnemyPatrolPoint* CurrentPoint = Cast<AEnemyPatrolPoint>(BBComp->GetValueAsObject("LocationToGo"));
+	AEnemyPatrolPoint* CurrentPoint = Cast<AEnemyPatrolPoint>(BBComp->GetValueAsObject("LocationToGo"));
 
-    TArray<AActor*> AvaliablePoints = AIContoller->GetPatrolPoints();
+	TArray<AActor*> AvaliablePoints = AIContoller->GetPatrolPoints();
 
-    AEnemyPatrolPoint* NextPoint = nullptr;
+	if (AvaliablePoints.Num() == 0) {
+		return EBTNodeResult::Failed;
+	}
 
-    if (AIContoller->CurrentPatrolPoint != AvaliablePoints.Num() - 1) {
-      NextPoint = Cast<AEnemyPatrolPoint>(AvaliablePoints[++AIContoller->CurrentPatrolPoint]);
-    }
-    else {
-      NextPoint = Cast<AEnemyPatrolPoint>(AvaliablePoints[0]);
-      AIContoller->CurrentPatrolPoint = 0;
-    }
+	AIContoller->CurrentPatrolPoint = (AIContoller->CurrentPatrolPoint + 1) % AvaliablePoints.Num();
 
-    BBComp->SetValueAsObject("LocationToGo", NextPoint);
-    return EBTNodeResult::Succeeded;
-  }
-  return EBTNodeResult::Failed;
+	AEnemyPatrolPoint* NextPoint = Cast<AEnemyPatrolPoint>(AvaliablePoints[AIContoller->CurrentPatrolPoint]);
+
+	BBComp->SetValueAsObject("LocationToGo", NextPoint);
+	return EBTNodeResult::Succeeded;
 }
